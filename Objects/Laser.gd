769088f6fe26434079a8b50fa9a9556360072711
meteor0id1 <meteorclop@gmail.com>
2022-02-05 21:09:13 -0500

    HTML

--- Objects/Laser.gd
+++ Objects/Laser.gd
@@ -14,7 +14,7 @@ func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
 
 
-func _on_Bullet_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+func _on_Bullet_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_index):
 	if (!self.is_queued_for_deletion() && body.is_in_group("Asteroids")):
 		body.call_deferred("explode")
 		get_parent().call_deferred("remove_child", self)
--- Objects/LaserWeapon.gd
+++ Objects/LaserWeapon.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-var laser_scene := load("res://objects/Laser.tscn")
+var laser_scene := load("res://Objects/Laser.tscn")
 
 func shoot():
 	var laser = laser_scene.instance()
 	laser.global_position = self.global_position
-	get_node("/root/World").add_child(laser)
+	get_parent().get_node("Label").text = get_parent().get_parent().name
+	get_parent().get_parent().add_child(laser)
